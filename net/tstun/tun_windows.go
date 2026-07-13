5e956183bd043f35ac677c80617e915844798cd7
andycloud689 <andycloud689@gmail.com>
2026-07-13 00:32:44 +0000

    Update tun_windows.go
    Signed-off-by: andycloud689 <andycloud689@gmail.com>

--- net/tstun/tun_windows.go
+++ net/tstun/tun_windows.go
@@ -1,22 +1,13 @@
 // Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-
-
-
 package tstun
-
-
-
 
 import (
 	"github.com/tailscale/wireguard-go/tun"
 	"golang.org/x/sys/windows"
 	"golang.zx2c4.com/wireguard/windows/tunnel/winipcfg"
 )
-
-
-
 
 func init() {
 	tun.WintunTunnelType = "Yuntailscale"
@@ -27,9 +18,6 @@ func init() {
 	tun.WintunStaticRequestedGUID = &guid
 }
 
-
-
-
 func interfaceName(dev tun.Device) (string, error) {
 	guid, err := winipcfg.LUID(dev.(*tun.NativeTun).LUID()).GUID()
 	if err != nil {
@@ -37,37 +25,3 @@ func interfaceName(dev tun.Device) (string, error) {
 	}
 	return guid.String(), nil
 }
-
-
-// Copyright (c) Tailscale Inc & contributors
-// SPDX-License-Identifier: BSD-3-Clause
-
-
-package tstun
-
-
-import (
-	"github.com/tailscale/wireguard-go/tun"
-	"golang.org/x/sys/windows"
-	"golang.zx2c4.com/wireguard/windows/tunnel/winipcfg"
-)
-
-
-func init() {
-	tun.WintunTunnelType = "Yuntailscale"
-	guid, err := windows.GUIDFromString("{37217669-42da-4657-a55b-0d995d328250}")
-	if err != nil {
-		panic(err)
-	}
-	tun.WintunStaticRequestedGUID = &guid
-}
-
-
-func interfaceName(dev tun.Device) (string, error) {
-	guid, err := winipcfg.LUID(dev.(*tun.NativeTun).LUID()).GUID()
-	if err != nil {
-		return "", err
-	}
-	return guid.String(), nil
-}
-
